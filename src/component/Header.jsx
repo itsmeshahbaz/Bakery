52fb4ca3dad385757b5b996a70b1831aeaef1fb8
M. Shahbaz <mshahbaz4283@gmail.com>
2025-07-02 22:52:25 +0500

    navbar update

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -34,9 +34,10 @@ function Header() {
       {menuOpen && (
         <div className="md:hidden bg-black/90 text-white px-6 py-4 space-y-4 absolute top-full left-0 w-full z-40">
           <Link to="/" className="block hover:text-yellow-300" onClick={() => setMenuOpen(false)}>Home</Link>
-          <div className="hover:text-yellow-300 cursor-pointer">Our Story</div>
-          <div className="hover:text-yellow-300 cursor-pointer">Products</div>
-          <div className="hover:text-yellow-300 cursor-pointer">Contact Us</div>
+          <Link to="/about" className="hover:text-yellow-300 cursor-pointer">About</Link>
+          <li className="hover:text-yellow-300 cursor-pointer">Our Story</li>
+          <Link to='/product' className="hover:text-yellow-300 cursor-pointer">Products</Link>
+          <Link to="/contactus" className="hover:text-yellow-300 cursor-pointer">Contact Us</Link>
         </div>
       )}
     </header>
